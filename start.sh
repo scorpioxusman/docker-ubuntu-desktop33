c617e7bf9e38f325cbb02da5d6567756f9a40b63
scorpioxusman <scorpiox256@gmail.com>
2025-12-30 16:31:24 +0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 
-# 1. Start Tailscale engine in the background
+# Start Tailscale engine
 /usr/sbin/tailscaled --tun=userspace-networking --socks5-server=localhost:1055 &
 
-# 2. Give the engine time to wake up
+# Wait for engine
 sleep 15
 
-# 3. Authenticate with your new REUSABLE key
-/usr/bin/tailscale up --auth-key=tskey-auth-kPuq2cnSWR11CNTRL-UNK3bNLctdL8EVNDp8CxdLfeYkiqXvwP --accept-dns=false --hostname=railway-desktop
+# Authenticate using the FULL key inside single quotes
+/usr/bin/tailscale up --auth-key='tskey-auth-kPuq2cnSWR11CNTRL-UNK3bNLctdL8EVNDp8CxdLfeYkiqXvwP' --accept-dns=false --hostname=railway-desktop
 
-# 4. Start Redis
+# Start services
 /usr/bin/redis-server --daemonize yes
-
-# 5. Clean up RDP and start it
 rm -f /var/run/xrdp/*.pid
 /usr/sbin/xrdp-sesman
 /usr/sbin/xrdp --nodaemon
