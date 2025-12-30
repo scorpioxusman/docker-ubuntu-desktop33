5660c5d635712130b7d160d8c30546376585752d
scorpioxusman <scorpiox256@gmail.com>
2025-12-30 16:35:16 +0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 
-# Start Tailscale engine
+# 1. Start Tailscale background engine
 /usr/sbin/tailscaled --tun=userspace-networking --socks5-server=localhost:1055 &
 
-# Wait for engine
+# 2. Wait 15 seconds for the engine to initialize
 sleep 15
 
-# Authenticate using the FULL key inside single quotes
-/usr/bin/tailscale up --auth-key='tskey-auth-kPuq2cnSWR11CNTRL-UNK3bNLctdL8EVNDp8CxdLfeYkiqXvwP' --accept-dns=false --hostname=railway-desktop
+# 3. Authenticate using the NEW key (Quotes prevent the dash error)
+/usr/bin/tailscale up --auth-key='tskey-auth-kd8uiJqS5511CNTRL-QSznzFWE6t9nVQ6GjEr9t9oGDGkgjzo4' --accept-dns=false --hostname=railway-desktop
 
-# Start services
+# 4. Start Redis in background
 /usr/bin/redis-server --daemonize yes
+
+# 5. Clear old RDP lock files
 rm -f /var/run/xrdp/*.pid
+
+# 6. Start RDP Session Manager
 /usr/sbin/xrdp-sesman
+
+# 7. Start XRDP (nodaemon keeps the container running)
 /usr/sbin/xrdp --nodaemon
